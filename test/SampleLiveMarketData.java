4cd2e9a935971d4ae12c7c60d16aa54075b2745a
DreamTeam <chris.smith@idnet.com>
2020-01-13 15:46:49 +0000

    if statement to prevent an attempt to set a price of an order which doesnt exist. Also put synchronized block only on setPrice() because was unnecessary on other method.

--- test/SampleLiveMarketData.java
+++ test/SampleLiveMarketData.java
@@ -20,6 +20,8 @@ public class SampleLiveMarketData extends Thread implements LiveMarketData
 
     private void setPrice()
     {
+        // In the case of setPrice being called twice simultaneously, the synchronized block forces only one setPrice() call at a time.
+        // Otherwise could get a stale price (no longer accurate);
         synchronized (this.order)
         {
             this.order.initialMarketPrice = 199 * RANDOM_NUM_GENERATOR.nextDouble();
@@ -31,17 +33,22 @@ public class SampleLiveMarketData extends Thread implements LiveMarketData
     {
         // Old set price method!
         System.out.println("SampleLiveMarketData run method called.");
-        setPrice();
+        if(this.order == null)
+        {
+            setPrice();
+        }
+        else
+        {
+            System.out.println("Cannot set price, no order provided! (null)");
+        }
+
     }
 
     @Override
     public void setPrice(Order o)
     {
         this.order = o;
-        synchronized (this.order)
-        {
-            setPrice();
-        }
+        setPrice();
     }
 
 }
