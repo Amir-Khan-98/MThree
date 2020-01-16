1a7696f5eeebf17306c82604d90effdb6093cfbf
NSarikas123 <nicholassarikas123@gmail.com>
2020-01-16 09:37:06 +0000

    Migrated main database functionality to own class, added seperate classes for seperate tables

--- src/Database/DatabaseRunMain.java
+++ src/Database/DatabaseRunMain.java
@@ -8,15 +8,27 @@ import Ref.Ric;
 
 public class DatabaseRunMain {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws Exception {
         DatabaseController dbc = new DatabaseController("com.mysql.jdbc.Driver","jdbc:mysql://localhost/marketdatabase","root","");
         //dbc.createOrderTable();
+        Connection databaseConnection = dbc.getConnection();
+
+        //String fix1 = "11=4;35=A;39=A";
+        //FixController.createFixTable(databaseConnection);
+        //FixController.addFixMessageToTable(databaseConnection,fix1);
+        // CODE FOR TESTING ORDER TABLE
+
         Instrument testInstrument = new Instrument(new Ric("BT.L"));
         Order testOrder = new Order(4,2,testInstrument,5);
         Order testOrder2 = new Order(6,7,testInstrument,10);
         //dbc.addOrderToTable(testOrder);
-        //DatabaseController.addOrderToTable(testOrder2);
-        DatabaseController.removeOrderFromTable(testOrder2);
+        OrderController.addOrderToTable(databaseConnection,testOrder);
+        OrderController.addOrderToTable(databaseConnection,testOrder2);
+        //OrderController.removeOrderFromTable(databaseConnection,testOrder2);
+
+        //
+
+        //CODE FOR TESTING FIX TABLE
 
     }
 }
